af40712da63f0752a422979d5e28480c1a454d12
alexellevu <newnapster83@gmail.com>
2021-06-23 17:51:26 +0200

    aggiunta altra faq

--- index.php
+++ index.php
@@ -18,6 +18,20 @@ $faqs =[
             
             Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'"
             
+        ],
+
+        [
+            'question'=> "Perché il mio account è associato a un paese?",
+            
+            'answer' =>"Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+
+            1.La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+            
+             a.Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+            
+                b.Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+            2La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
         ]
 
 
