452ccb16afd04067d537c20f86f1d56a7beed3c9
alexellevu <newnapster83@gmail.com>
2021-06-23 15:51:28 +0200

    inizio struttura dati

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<?php 
+$faq =[
+    [
+        "'Come state implementando la recente decisione della Corte di giustizia           dell'Unione europea (CGUE) relativa al diritto all'oblio?'"
+        => 
+        "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi."
+     
+    ],
+
+    [
+        "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?"
+        =>
+        "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno."
+
+    ]
+
+
+
+]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
